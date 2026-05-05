ea60e31a881a6d426f8ee7c30dd5140902d8784b
Toufik Saouchi <toufik.saouchi@gmail.com>
2026-05-05 17:38:08 +0200

    example

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import os
+from pprint import pprint
+from typing import cast
+
+import dotenv
+
+from pricelab_retriever.adapter.outbound.configuration.load_configuration import LoadConfiguration
+
+
 def main():
-    print("Hello friend, happy coding")
+    dotenv.load_dotenv()
+    app_configuration = LoadConfiguration(
+        cast(str, os.getenv("CONFIGURATION_FILE_PATH"))
+    ).load()
+    pprint(app_configuration)
 
 if __name__ == "__main__":
     main()
