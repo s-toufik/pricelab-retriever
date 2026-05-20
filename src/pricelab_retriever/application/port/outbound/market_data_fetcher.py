f9b53bf08654e82dffe9016e4203b056c6d3cc5d
Toufik Saouchi <toufik.saouchi@gmail.com>
2026-05-20 22:22:29 +0200

    style(market_data_fetcher): remove extra blank line after import statement
    - Deleted unnecessary blank line after import in market_data_fetcher module
    - Kept code formatting consistent with project style guidelines

--- src/pricelab_retriever/application/port/outbound/market_data_fetcher.py
+++ src/pricelab_retriever/application/port/outbound/market_data_fetcher.py
@@ -2,5 +2,6 @@ from typing import Protocol, Dict, Sequence
 
 from pricelab_core.domain.model.candles.candle import Candle
 
+
 class MarketDataFetcher(Protocol):
-    async def fetch_intraday(self, symbol: str, params: Dict[str, str]) ->  Sequence[Candle]: ...
+    async def fetch_intraday(self, symbol: str, params: Dict[str, str]) -> Sequence[Candle]: ...
